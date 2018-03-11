fa2d64f31d06d394d467d5d212c4a4d71ca4f92b
devangmotwani <devang.motwani16@gmail.com>
2018-03-11 16:28:26 -0400

    Money_change done the greedy way

--- Week-3/1_money_change/change.py
+++ Week-3/1_money_change/change.py
@@ -3,8 +3,12 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    tens=int(m/10)
+    temp=float(m%10)
+    fives=int(temp/5)
+    ones=float(temp%5)
+    return tens+fives+int(ones)
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())#sys.stdin.read())
     print(get_change(m))
