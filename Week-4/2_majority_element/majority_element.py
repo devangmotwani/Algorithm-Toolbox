d000a5df281e1cbf7a8d71eb45a5aa0d4ced248d
devangmotwani <devang.motwani16@gmail.com>
2018-03-24 19:17:47 -0400

    Majority element. Iterative

--- Week-4/2_majority_element/majority_element.py
+++ Week-4/2_majority_element/majority_element.py
@@ -7,11 +7,27 @@ def get_majority_element(a, left, right):
     if left + 1 == right:
         return a[left]
     #write your code here
-    return -1
+    count={}
+    for i in range(left,right):
+        if a[i] in count:
+            count[a[i]]+=1
+        else:
+            count[a[i]]=1
+    max_count=(list(count.values()))
+    #print(max_count)
+    m_count=max(max_count)
+    if m_count> ((right+left)/2):
+        return 1
+    else:
+        return -1
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
+    input_cmd = input()#sys.stdin.read()
+    input_cmd2 = input()
+    n = list(map(int, input_cmd.split())) 
+    n=n.pop()
+    a = list(map(int, input_cmd2.split()))
+    #print(n)
     if get_majority_element(a, 0, n) != -1:
         print(1)
     else:
